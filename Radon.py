2b4e7980bc6cccda1ea59c97b68c3a5cf8f7c7a5
lawruble13 <liam@wrubleski.net>
2020-08-18 10:43:31 -0600

    Running different trials

--- Radon.py
+++ Radon.py
@@ -13,6 +13,7 @@ from sklearn.linear_model import LinearRegression
 
 LRn222 = 3.825 * 24 * 60 * 60  # the half-life for Rn-222 (in seconds)
 LPo218 = 3.05 * 60  # the half-life for Po-218
+LPb210 = (26.8 * 60) + (19.7 * 60) + (1.5e-4)  # the half-lives for Pb-214, Bi-214, and Po-214
 DC1HL = np.array([LRn222, LPo218])
 DC1Lambda = np.log(2) / DC1HL  # the decay constants for this first decay chain in units of 1/min
 
@@ -21,45 +22,70 @@ LPo216 = 0.158  # the half-life for Po-216
 DC2HL = np.array([LRn220, LPo216])
 DC2Lambda = np.log(2) / DC2HL  # the decay constants for this first decay chain in units of 1/min
 
+time_to_run = 900;
+sample_time = 15;
+n_samples = time_to_run // sample_time;
+
 
 def gen_inputs(*rates):
-    proportions = [[0] * 3 for _ in range(12)]
-    for i in range(12):
+    proportions = [[0] * 3 for _ in range(n_samples)]
+    for i in range(n_samples):
         for j in range(len(rates)):
             for r in range(j + 1):
                 tmp = 1
                 for q in range(j + 1):
                     if q != r:
                         tmp *= rates[q] / (rates[q] - rates[r])
-                tmp *= np.exp(-rates[r] * 15 * i) - np.exp(-rates[r] * 15 * (i + 1))
+                tmp *= np.exp(-rates[r] * i) - np.exp(-rates[r] * (i + 1))
                 proportions[i][j] += tmp
-    return [sum(p) for p in proportions]
+    inputs = [sum(p) for p in proportions]
+    return inputs  # /inputs[0]
 
 
 def expcount(n, *args):
-    countlist = np.array([0] * 12)
+    countlist = np.array([0] * n_samples)
 
     for _ in range(n):
-        templist = [int(x) // 15 for x in np.cumsum([rnd.exponential(a) for a in args])]
+        templist = [int(x) for x in np.cumsum([rnd.exponential(a) for a in args])]
 
         for j in templist:
-            if j < 12:
+            if j < len(countlist):
                 countlist[j] = countlist[j] + 1
     return countlist
 
 
-k = 10
-n1_est = [0] * k
-n2_est = [0] * k
-for i in range(k):
-    n1 = 100000
-    n2 = 50000
-    counts = np.array(expcount(n1, *(1 / DC1Lambda))) + np.array(expcount(n2, *(1 / DC2Lambda)))
-    input1 = gen_inputs(*DC1Lambda)
-    input2 = gen_inputs(*DC2Lambda)
+time_to_run = 60;
+sample_time = 15;
+n_samples = time_to_run // sample_time;
 
-    inputs = np.transpose(np.vstack((input1, input2)))
-    estimated_amounts = LinearRegression(fit_intercept=False).fit(inputs, counts)
-    n1_est[i], n2_est[i] = estimated_amounts.coef_
-print(n1_est)
-print(n2_est)
+k = 5
+n1_mean = [[0] * 15 for _ in range(60)]
+n1_stdv = [[0] * 15 for _ in range(60)]
+n2_mean = [[0] * 15 for _ in range(60)]
+n2_stdv = [[0] * 15 for _ in range(60)]
+
+for i in range(60):
+    sample_time = i + 1
+    for j in range(15):
+        time_to_run = 60 * (j + 1)
+        n_samples = time_to_run // sample_time;
+        n1_est = []
+        n2_est = []
+        for _ in range(k):
+            n1 = 100000
+            n2 = 50000
+            counts = np.array(expcount(n1, *(1 / DC1Lambda))) + np.array(expcount(n2, *(1 / DC2Lambda)))
+            input1 = gen_inputs(*DC1Lambda)
+            input2 = gen_inputs(*DC2Lambda)
+
+            inputs = np.transpose(np.vstack((input1, input2)))
+            estimated_amounts = LinearRegression(fit_intercept=False).fit(inputs, counts)
+            n1_est.append(estimated_amounts.coef_[0])
+            n2_est.append(estimated_amounts.coef_[1])
+        n1_mean[i][j] = np.mean(n1_est)
+        n1_stdv[i][j] = np.std(n1_est)
+        n2_mean[i][j] = np.mean(n2_est)
+        n2_mean[i][j] = np.std(n2_est)
+        print("With sample time {}s and run time {}s:".format(sample_time, time_to_run))
+        print("\tThe mean for Rn222 is {0} with standard deviation {1}".format(np.mean(n1_est), np.std(n1_est)))
+        print("\tThe mean for Rn220 is {0} with standard deviation {1}".format(np.mean(n2_est), np.std(n2_est)))
--- radon.m
+++ radon.m
@@ -1,11 +1,27 @@
 clear;
 close all;
 
-t = 0:15:165;
-% For Rn-220
-half_lives=[54.5 0.158];
+t = 0:15:3000;
+
+% For Rn-222
+half_lives=[3.825*24*60*60 3.05*60 ];%26.8*60+19.7*60+1.5e-4];
 counts = zeros(length(t), length(half_lives));
 for i = 1:length(t)
     counts(i,:) = decay_interval(t(i), t(i)+15, half_lives);
-    fprintf("t: %d-%d : %d counts", t(i), t(i)+15, sum(counts(i,:)));
+    fprintf("t: %d-%d : %d counts\n", t(i), t(i)+15, sum(counts(i,:)));
 end
+
+sum_counts = sum(counts,2);
+plot(t, sum_counts./sum_counts(1))
+hold on;
+
+% For Rn-220
+half_lives = [54.5 0.158];
+counts = zeros(length(t), length(half_lives));
+for i=1:length(t)
+    counts(i,:) = decay_interval(t(i), t(i)+15, half_lives);
+end
+sum_counts = sum(counts,2);
+plot(t, sum_counts./sum_counts(1))
+
+
