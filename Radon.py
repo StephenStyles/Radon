040de5e7441880fe7eef65f11874f20cc6ad70d3
StephenStyles <51934547+StephenStyles@users.noreply.github.com>
2020-08-19 09:13:36 -0600

    Update Radon.py

--- Radon.py
+++ Radon.py
@@ -9,6 +9,38 @@ import numpy as np
 import numpy.random as rnd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+
+
+def expcount(n,time,step, *args):
+    countlist = np.array([0]*int((time/step)))
+    
+    for _ in range(n):
+        templist=[int(x)//step for x in np.cumsum([rnd.exponential(a) for a in args])]
+        
+        for j in templist:
+            if j <(time/step):
+                countlist[j] = countlist[j]+1
+    return countlist
+
+def get_inputs(time,step, *args):
+    
+    #times = [0,15,30,45,60,75,90,105,120,135,150,165,180]
+    times = list(range(0,int(time+step),int(step)))
+    inputs = [0]*int((time/step))
+    for i in range(int(time/step)):
+        inputs[i] = inputs[i]+(np.exp(-args[0]*times[i])-np.exp(-args[0]*times[i+1]))
+        progeny_sum = 0
+        for r in range(len(args)):
+            tmp=1
+            for q in range(len(args)):
+                if q==r:
+                    continue
+                tmp *= args[q]/(args[q]-args[r])
+            progeny_sum = progeny_sum + tmp*(np.exp(-args[r]*times[i])-np.exp(-args[r]*times[i+1]))
+        inputs[i] = inputs[i]+progeny_sum
+    return inputs
 
 LRn222 = 3.825*24*60*60 # the half-life for Rn-222 (in seconds)
 LPo218 = 3.05*60        # the half-life for Po-218
@@ -22,55 +54,88 @@ DC2HL = np.array([LRn220,LPo216])
 DC2Lambda = np.log(2)/DC2HL # the decay constants for this first decay chain in units of 1/min
 DC2Lambda
 
-def gen_inputs(n, *args):
-    inputs = [[0]*len(args)]*12
-    for i in range(12):
-        inputs[i][0] = np.exp(-args[0]*15*(i+1))
-        for j in range(1,len(args)):
-            inputs[i][j] = 0
-            for r in range(j+1):
-                tmp = 1
-                for q in range(j+1):
-                    if q == r:
-                        continue
-                    tmp *= args[q]/(args[q]-args[r])
-                tmp *= args[r]
-                tmp *= np.exp(-args[r]*15*(i+1))
-                inputs[i][j] += tmp
-            inputs[i][j] /= args[j]
-    return inputs
+n=8000
+m=8000
 
-def expcount(n, *args):
-    countlist = np.array([0]*12)
+step = 60
+time = 3600
+counts = expcount(n,time,step,1/DC1Lambda) + expcount(m,time,step,1/DC2Lambda)
+
+X = np.array([get_inputs(time,step,*(DC1Lambda)), get_inputs(time,step,*(DC2Lambda))]).transpose()
+
+
+#indices=[0,6,len(counts)-1]
+#fitX = [X[index] for index in indices]
+#fitY = [counts[index] for index in indices]
+
+
+Rn222 = []
+Rn220 = []
+
+for _ in range(10):
+    counts = expcount(n,time,step,1/DC1Lambda) + expcount(m,time,step,1/DC2Lambda)
+    reg = LinearRegression(fit_intercept = False).fit(X, counts)
+    Rn222.append(reg.coef_[0])
+    Rn220.append(reg.coef_[1])
     
-    for _ in range(n):
-        templist=[int(x)//12 for x in np.cumsum([rnd.exponential(a) for a in args])]
+print("The mean for Rn222 is {0} with standard deviation {1}".format(np.mean(Rn222), np.std(Rn222)))
+print("The mean for Rn220 is {0} with standard deviation {1}".format(np.mean(Rn220), np.std(Rn220)))
+
+
+time = list(range(300,1500,60))
+step = [10,15,20,30,60]
+
+
+stdmatrix = np.zeros((len(time),len(step)))
+
+for i in range(len(time)):
+    for j in range(len(step)):
+        X = np.array([get_inputs(time[i],step[j],*(DC1Lambda)), get_inputs(time[i],step[j],*(DC2Lambda))]).transpose()
+
+        Rn222 = []
+
+        for _ in range(5):
+            counts = expcount(n,time[i],step[j],1/DC1Lambda) + expcount(m,time[i],step[j],1/DC2Lambda)
+            reg = LinearRegression(fit_intercept = False).fit(X, counts)
+            Rn222.append(reg.coef_[0])
         
-        for j in templist:
-            if j <12:
-                countlist[j] = countlist[j]+1
-    return countlist
+        tmp = np.std(Rn222)
+        print(tmp)   
+        stdmatrix[i,j] = tmp
+stdmatrix
+np.savetxt("std.txt",stdmatrix)
+
+from mpl_toolkits.mplot3d import Axes3D
+
+X2, Y2 = np.meshgrid(step, time)
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+ax.plot_surface(X2, Y2, stdmatrix, cmap="coolwarm")
+
+plt.show()
 
 
-n=10000
+
+n = 10
+step = 60
+time = 600
+inputs = []
+times = list(range(0,int(time+step),int(step)))
+inputs = [0]*int((time/step))
+for i in range(int(time/step)):
+    inputs[i] = inputs[i]+(np.exp(-DC1Lambda[1]*times[i])-np.exp(-DC1Lambda[1]*times[i+1]))
+
+
+counts = expcount(n,time,step,1/DC1Lambda[1])
+X = np.array([inputs]).transpose()
+reg = LinearRegression(fit_intercept = False).fit(X, counts)
+reg.coef_[0]
+
+Po218 = []
+
+for _ in range(100):
+   counts = expcount(n,time,step,1/DC1Lambda[1])
+   reg = LinearRegression(fit_intercept = False).fit(X, counts)
+   Po218.append(reg.coef_[0])
     
-counts = expcount(n,1/DC1Lambda) + expcount(n,1/DC2Lambda)
-gen_inputs(n,1,2)
-
-def get_inputs(*args):
-    inputs = [[0]]*12
-    times = [0,15,30,45,60,75,90,105,120,135,150,165,180]
-    for i in range(12):
-        inputs[i] = inputs[i]+(np.exp(-args[0]*times[i+1])-np.exp(-args[0]*times[i]))
-        progeny_sum = 0
-        for r in range(len(args)):
-            tmp=1
-            for q in range(len(args)):
-                if q==r:
-                    continue
-                tmp *= args[q]/(args[q]-args[r])
-            progeny_sum = progeny_sum + tmp*(np.exp(-args[r]*times[i+1])-np.exp(-args[r]*times[i]))
-        inputs[i] = inputs[i]+progeny_sum
-    return inputs
-        
-get_inputs(4,5)
+print("The mean for Po218 is {0} with standard deviation {1}".format(np.mean(Po218), np.std(Po218)))
